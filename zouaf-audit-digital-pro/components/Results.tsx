6c101a1675ddba0bb332c22c157015b2db968f5b
Zouaf-team <contact@zouaf.com>
2025-12-10 15:43:59 +0100

    Update Results.tsx

--- zouaf-audit-digital-pro/components/Results.tsx
+++ zouaf-audit-digital-pro/components/Results.tsx
@@ -86,16 +86,16 @@ export const Results: React.FC<ResultsProps> = ({ result, userName }) => {
               href="https://app.zouaf.pro/souscription" 
               target="_blank" 
               rel="noopener noreferrer"
-              className="inline-block bg-zouaf-orange text-white font-bold text-xl px-10 py-4 rounded-full shadow-lg shadow-zouaf-orange/40 hover:bg-[#E67A58] hover:-translate-y-1 transition-all"
+              className="inline-block bg-zouaf-orange text-white font-bold text-xl px-10 py-4 rounded-xl shadow-lg shadow-zouaf-orange/40 hover:bg-[#E67A58] hover:-translate-y-1 transition-all"
             >
               Découvrir Zouaf Gratuitement
-             <p className="mt-4 text-sm text-gray-400">Sans ajout d'informations bancaires</p>
             </a>
+             <p className="mt-4 text-sm text-gray-400">Sans ajout d'informations bancaires</p>
               <a 
               href="https://app.lemcal.com/@zouaf" 
               target="_blank" 
               rel="noopener noreferrer"
-              className="inline-block bg-zouaf-orange text-white font-bold text-xl px-10 py-4 rounded-full shadow-lg shadow-zouaf-orange/40 hover:bg-[#E67A58] hover:-translate-y-1 transition-all"
+              className="inline-block bg-zouaf-orange text-white font-bold text-xl px-10 py-4 rounded-xl shadow-lg shadow-zouaf-orange/40 hover:bg-[#0c343d] hover:-translate-y-1 transition-all"
             >
               Réserver une démo
             </a>
